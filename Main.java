d325aee0e531f8ef9bb387ceef9cea6d0b54b26a
ChoSooBeen <100505030+ChoSooBeen@users.noreply.github.com>
2022-03-04 18:48:18 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package test;
+import java.util.*;
+
+public class Main {
+    static Scanner s = new Scanner(System.in);
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args){
+        int input = s.nextInt();
+        ArrayList<Integer> a = new ArrayList<>();
+        for(int i = 0; i < input; i++){
+            a.add(s.nextInt());
+        }
+        Collections.sort(a);
+
+        for(int i : a) {
+            sb.append(i).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
