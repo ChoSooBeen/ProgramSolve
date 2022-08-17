453c8fa2a777f1e1ac080625027bc30a6d626666
ChoSooBeen <100505030+ChoSooBeen@users.noreply.github.com>
2022-08-17 12:12:36 +0900

    Create 2922.java

---
+++ 2922.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class math_2292 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+        int sum = 1;
+        int count = 1;
+
+        if(num == 1)
+            System.out.println(1);
+        else {
+            while (true) {
+                sum = sum + 6 * count;
+                if (num <= sum) {
+                    System.out.println(count + 1);
+                    break;
+                }
+                count++;
+            }
+        }
+    }
+}
