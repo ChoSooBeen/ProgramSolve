1ee8a950501595199f03c39daf7e3baa81e611e5
ChoSooBeen <100505030+ChoSooBeen@users.noreply.github.com>
2022-09-29 11:32:13 +0900

    Create 10757.java

---
+++ 10757.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class marathon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] s = scanner.nextLine().split(" ");
+
+        BigInteger A = new BigInteger(s[0]);
+        BigInteger B = new BigInteger(s[1]);
+
+        System.out.println(A.add(B));
+    }
+}
