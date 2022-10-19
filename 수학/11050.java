26503628c5c0a2c6bb36e88699e1a379e16ec409
ChoSooBeen <100505030+ChoSooBeen@users.noreply.github.com>
2022-10-19 10:39:37 +0900

    Create 11050.java

---
+++ 수학/11050.java
@@ -0,0 +1,26 @@
+//이항계수1
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = bf.readLine().split(" ");
+
+        int N = Integer.parseInt(s[0]);
+        int K = Integer.parseInt(s[1]);
+        int nk = N - K;
+
+        int n_fac = 1;
+        int k_fac = 1;
+        int nk_fac = 1;
+
+        for(int i = N; i > 0; i--) {
+            n_fac *= i;
+            if(i <= K)
+                k_fac *= i;
+            if(i <= nk)
+                nk_fac *= i;
+        }
+        System.out.println(n_fac/(k_fac * nk_fac));
+    }
+}
