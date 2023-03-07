0db212f6779295f3785d942c3b3b84d96a1db516
ChoSooBeen <100505030+ChoSooBeen@users.noreply.github.com>
2023-03-07 10:23:36 +0900

    1427번 - 소트인사이드(문자열 정렬)

--- Sort/1427.java
+++ Sort/1427.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.nextLine();
