d8c58e4c1b4f780133deb20b737bfb5c34f1db4d
조수빈 <100505030+ChoSooBeen@users.noreply.github.com>
2023-11-04 14:49:03 +0900

    11656번 - 접미사 배열

---
+++ String/11656.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class BOJ11656 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+        int length = input.length();
+
+        String[] suffix = new String[length];
+        for(int i = 0; i < length; i++) {
+            suffix[i] = input.substring(i);
+        }
+        Arrays.sort(suffix);
+        for(int i = 0; i < length; i++) {
+            bw.write(suffix[i] + '\n');
+        }
+        bw.flush();
+    }
+}
